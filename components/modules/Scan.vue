a154fb6353213cb2d212b3f026c0a9acb1e101d1
jmherrera <jerreramarcia@gmail.com>
2025-03-29 00:10:46 +0100

    Camera option

--- components/modules/Scan.vue
+++ components/modules/Scan.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="bg-blue-200 rounded-xl p-4 text-center shadow">
-    <h2 class="text-xl font-bold">SCAN</h2>
+  <div class="bg-blue-200 rounded-xl p-4 text-center shadow cursor-pointer">
+    <p class="font-bold text-xl mb-2" @click="openCamera">SCAN</p>
+    <video
+        ref="video"
+        autoplay
+        playsinline
+        muted
+        controls
+        class="mx-auto mt-2 rounded-lg"
+        :class="{ 'hidden': !cameraOn }"
+    />
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const video = ref(null)
+const cameraOn = ref(false)
+
+const openCamera = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: 'environment' // Usa cámara trasera si está disponible
+      },
+      audio: false
+    })
+    video.value.srcObject = stream
+    cameraOn.value = true
+  } catch (err) {
+    console.error('No se pudo acceder a la cámara:', err)
+    alert('No se pudo acceder a la cámara.')
+  }
+}
+</script>
